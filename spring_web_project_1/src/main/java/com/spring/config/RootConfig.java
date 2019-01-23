2cd2194405ca171f41f09c7d4b76fe61c2275a08
Seong Hyeok Lee <dltjdgur327@gmail.com>
2019-01-24 00:43:29 +0900

    config 설정

--- spring_web_project_1/src/main/java/com/spring/config/RootConfig.java
+++ spring_web_project_1/src/main/java/com/spring/config/RootConfig.java
@@ -1,8 +1,26 @@
 package com.spring.config;
 
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@ComponentScan(basePackages= {"com.spring"})
 public class RootConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		dataSource.setUrl("jdbc:log4jdbc:mysql://127.0.0.1:3306/study_db?serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("mysqlpwd");
+
+		return dataSource;
+	}
 
 }

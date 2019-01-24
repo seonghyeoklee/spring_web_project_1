b1df121c02d8886f1394fbbe8035116f02d6e8c7
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-24 17:05:42 +0900

    java config

---
+++ spring_web_project_1/src/main/java/com/spring/config/ServletConfig.java
@@ -0,0 +1,56 @@
+package com.spring.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.spring"})
+public class ServletConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean(name="multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+
+		//한번의 Request로 전달될 수 있는 최대의 크기
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+
+		//하나의 파일 최대 크기
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+
+		//메모리상에서 유지되는 최대의 크기
+		resolver.setMaxInMemorySize(1024 * 1024);
+
+		//이상의 크기에서는 임시파일 형태로 보관
+		resolver.setUploadTempDir(new FileSystemResource("C:\\upload\\tmp"));
+		resolver.setDefaultEncoding("UTF-8");
+
+		return resolver;
+	}
+
+}

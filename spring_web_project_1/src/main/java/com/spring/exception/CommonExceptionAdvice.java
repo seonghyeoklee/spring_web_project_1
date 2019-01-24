b1f3cf6f5809a19b220f513ebf3e2f7052a8f9ec
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-24 17:06:07 +0900

    exception

---
+++ spring_web_project_1/src/main/java/com/spring/exception/CommonExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.spring.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.log4j.Log4j;
+
+@ControllerAdvice
+@Log4j
+public class CommonExceptionAdvice {
+
+	@ExceptionHandler(Exception.class)
+	public String exception(Exception ex, Model model) {
+
+		model.addAttribute("exception", ex);
+		return "error";
+	}
+}

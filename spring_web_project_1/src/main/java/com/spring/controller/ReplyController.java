23df7644d467cea3a2c8e8b3660ceb74f67acfe7
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-06-19 17:58:35 +0900

    댓글 추가, 조회, 삭제

--- spring_web_project_1/src/main/java/com/spring/controller/ReplyController.java
+++ spring_web_project_1/src/main/java/com/spring/controller/ReplyController.java
@@ -1,13 +1,19 @@
 package com.spring.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.model.Criteria;
 import com.spring.model.Reply;
 import com.spring.service.ReplyService;
 
@@ -30,6 +36,24 @@ public class ReplyController {
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@GetMapping(value="/pages/{bno}/{page}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<Reply>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+		Criteria cri = new Criteria(page, 10);
 
+		return new ResponseEntity<>(replyService.getList(cri, bno), HttpStatus.OK);
+	}
 
+	@GetMapping(value="/{rno}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<Reply> get(@PathVariable("rno") Long rno){
+
+		return new ResponseEntity<>(replyService.get(rno), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value="/{rno}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+
+		int deleteCount = replyService.remove(rno);
+
+		return deleteCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
